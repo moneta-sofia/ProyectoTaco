4a6ca797abc9c8d2e0a303af1e1e411304171b3f
elxrojo <elxrojo115@gmail.com>
2024-08-10 11:04:29 -0300

    changing vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ command }) => {
-	const isProduction = command === 'build';
-	const baseUrl = isProduction ? '/your-production-path/' : '/';
-
-	return {
-		base: baseUrl,
-		// other configurations...
-	};
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
 });
