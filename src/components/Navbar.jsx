25180f0e2d27568698737e8a2a53487611c3e4af
elxrojo <elxrojo115@gmail.com>
2025-01-19 14:09:30 -0300

    added category selected

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,52 @@
 import { IoMenu, IoCloseOutline } from 'react-icons/io5';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
 	const [isOpen, setIsOpen] = useState(false);
 
 	return (
 		<>
-			<div className='self-start z-10 pointer-events-auto flex flex-col items-end'>
+			<div className="self-start z-10 pointer-events-auto flex flex-col items-end">
 				<button className=" bg-black p-4 rounded-full w-fit cursor-pointer select-none text-3xl flex xl:flex-row flex-col xl:items-center items-start align-center text-white z-40" onClick={() => setIsOpen(!isOpen)}>
-                {/* border-solid border-2 border-white */}
-					{isOpen ? <IoCloseOutline  /> : <IoMenu  />}
-					{/* <p className="text-black ">Hi !</p> */}
+					{isOpen ? <IoCloseOutline /> : <IoMenu />}
 				</button>
 				{isOpen && (
 					<>
 						<div className={`blur w-screen h-screen fixed inset-0 ${isOpen ? 'md:hidden flex' : 'hidden'}  `}></div>
 						<div className="w-fit h-fit bg-black p-4 mt-3 rounded-xl text-end">
-							<Link activeClass="active" to="../backgroundDesign" onClick={() => setIsOpen(false)} className=" text-white cursor-pointer">
+
+							<NavLink 
+							activeClass="active" 
+							to="/backgroundDesign" 
+							onClick={() => setIsOpen(false)} 
+							className=" text-white cursor-pointer">
 								Background Design
-							</Link>
+							</NavLink>
 							<hr className="w-full my-2" />
-							<Link activeClass="active" to="../characterDesign" onClick={() => setIsOpen(false)} className=" text-white cursor-pointer ">
+
+							<NavLink activeClass="active" 
+							to="/characterDesign"
+							onClick={() => setIsOpen(false)}
+							 className=" text-white cursor-pointer ">
 								Character Design
-							</Link>
+							</NavLink>
 							<hr className="w-full my-2" />
-							<Link activeClass="active" to="../animation" onClick={() => setIsOpen(false)} className=" text-white cursor-pointer">
+
+							<NavLink activeClass="active" 
+							to="/animation"
+							onClick={() => setIsOpen(false)}
+							 className=" text-white cursor-pointer">
 								Animation
-							</Link>
+							</NavLink>
 							<hr className="w-full my-2" />
-							<Link activeClass="active" to="../illustration" onClick={() => setIsOpen(false)} className=" text-white cursor-pointer">
+
+							<NavLink activeClass="active" 
+							to="/illustration"
+							onClick={() => setIsOpen(false)}
+							 className=" text-white cursor-pointer">
 								Ilustration
-							</Link>
+							</NavLink>
 						</div>
 					</>
 				)}
