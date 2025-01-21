7cf57dae3a89963921904c33e3c78d40f557716d
elxrojo <elxrojo115@gmail.com>
2025-01-21 14:03:46 -0300

    added animation to navbar button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,14 +7,14 @@ export default function Navbar() {
 
 	return (
 		<>
-			<div className="self-start z-10 pointer-events-auto flex flex-col items-end">
-				<button className=" bg-black p-4 rounded-full w-fit cursor-pointer select-none text-3xl flex xl:flex-row flex-col xl:items-center items-start align-center text-white z-40" onClick={() => setIsOpen(!isOpen)}>
+			<div className="transition-transform  self-start z-10 pointer-events-auto flex flex-col items-end">
+				<button className={` transition-transform  bg-black p-4 rounded-full w-fit cursor-pointer select-none text-3xl flex xl:flex-row flex-col xl:items-center items-start align-center text-white z-40 ${isOpen ? "rotate" : ""}`} onClick={() => setIsOpen(!isOpen)}>
 					{isOpen ? <IoCloseOutline /> : <IoMenu />}
 				</button>
 				{isOpen && (
 					<>
 						<div className={`blur w-screen h-screen fixed inset-0 ${isOpen ? 'md:hidden flex' : 'hidden'}  `}></div>
-						<div className="w-fit h-fit bg-black p-4 mt-3 rounded-xl text-end">
+						<div className="w-fit h-fit bg-black p-4 mt-3 rounded-xl text-end transition-transform ">
 							<NavLink activeClass="active" to="/backgroundDesign" onClick={() => setIsOpen(false)} className=" text-white cursor-pointer">
 								Background Design
 							</NavLink>
