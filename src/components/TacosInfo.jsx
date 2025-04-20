47af25ac64c4d16fdd40907f44c8a9c3a0a3345c
elxrojo <elxrojo115@gmail.com>
2025-04-20 15:14:08 +0200

    feat: add contact buttom

--- src/components/TacosInfo.jsx
+++ src/components/TacosInfo.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { FaYoutube, FaInstagram, FaLinkedin } from 'react-icons/fa';
 import { FaTiktok, FaBluesky } from 'react-icons/fa6';
-import { RiTwitterXFill } from 'react-icons/ri';
+import { IoDocumentTextOutline } from 'react-icons/io5';
+// import { RiTwitterXFill } from 'react-icons/ri';
 // import emailjs from '@emailjs/browser';
 // import toast, { Toaster } from 'react-hot-toast';
 import ModalTacoInfo from './ModalTacoInfo';
@@ -14,12 +15,12 @@ export default function TacosInfo() {
 	// 	e.preventDefault();
 	// 	toast
 	// 		.promise(
-    //             emailjs.sendForm("service_taco", "template_mxgw4ua", form.current, "GRinZEICKiSrZx_GT"),
-    //             {
-    //                 loading: 'Sending...',
-    //                 success: <b>Mail sended!</b>,
-    //                 error: <b>Could not send the mail :(</b>,
-    //             })
+	//             emailjs.sendForm("service_taco", "template_mxgw4ua", form.current, "GRinZEICKiSrZx_GT"),
+	//             {
+	//                 loading: 'Sending...',
+	//                 success: <b>Mail sended!</b>,
+	//                 error: <b>Could not send the mail :(</b>,
+	//             })
 	// 		.then(
 	// 			() => {
 	// 				form.current.reset();
@@ -34,10 +35,16 @@ export default function TacosInfo() {
 		<>
 			{/* <Toaster position="bottom-center" /> */}
 			<section className="w-full flex flex-col items-center z-30">
-			{modalInfo && <ModalTacoInfo setModalInfo={setModalInfo} />}
+				{modalInfo && <ModalTacoInfo setModalInfo={setModalInfo} />}
 				<div className="infos-height flex flex-col justify-center items-center">
 					<h1 className="text-5xl my-3">DIEGO M. RUIZ</h1>
-					<button onClick={()=> setModalInfo(true)}  className="px-4 py-1 border-solid border-white border-2 rounded-xl my-5 text-2xl hover:bg-stone-800 active:bg-transparent"> About me</button>
+					<div className="flex items-center">
+						<button onClick={() => setModalInfo(true)} className=" leading-none px-4 py-2 border-solid border-white rounded-xl my-5 text-2xl hover:bg-stone-800 active:bg-transparent" style={{ border: '3px solid' }}>
+							{' '}
+							About me
+						</button>
+						{/* <IoDocumentTextOutline href="/CV-Taco.pdf" download={''} color="white" size={40} className=" cursor-pointer mx-2" /> */}
+					</div>
 				</div>
 				<div className="flex w-80 mb-8 justify-around items-center">
 					<a href="https://www.youtube.com/@Tacoelshido">
@@ -59,7 +66,13 @@ export default function TacosInfo() {
 						<FaBluesky size={28} color="white" />
 					</a>
 				</div>
-{/* 
+
+				<a  className="px-6 py-2 flex items-center justify-center border-solid border-white bg-white rounded-xl my-5 text-2xl hover:bg-slate-300 active:bg-transparent leading-none mb-14" style={{ border: '3px solid' }} 
+				href="https://mail.google.com/mail/?view=cm&fs=1&to=tacoelshido@gmail.com"
+				target="_blank">
+					<p className='leading-none text-black p-0 m-0' >Contact</p>
+				</a>
+				{/* 
 				<form ref={form} onSubmit={handlerEmail} className="xl:w-4/12 w-11/12 flex flex-col justify-center items-end xl:mt-16 xl:mb-16 mt-16 mb-36">
 					<input name="user_name"placeholder='Name' type="text" className="bg-transparent placeholder:text-white text-left text-2xl my-3 w-full border-2 border-solid rounded-xl py-3 px-5" />
 					<input name="user_email"placeholder='Email' type="email" className="bg-transparent placeholder:text-white text-left text-2xl my-3 w-full border-2 border-solid rounded-xl py-3 px-5" />
